817f78de800a57b13fbb787a847cd0df3daf8935
nasah01 <57249852+nasah01@users.noreply.github.com>
2024-12-11 20:42:24 +0530

    Add files via upload

--- javascript/hhm-javascript.js
+++ javascript/hhm-javascript.js
@@ -1,6 +1,7 @@
-// On click, function to open Popup received as parameter
-function openPopup(Popupid) {
-  var popup = document.getElementById(Popupid);
-  popup.classList.toggle("show");
+function openpopup(mylink, windowname) { if (! window.focus)return true; var href; if (typeof(mylink) == 'string') href=mylink; else href=mylink.href; window.open(href, windowname, 'width=800,height=500,left=300,top=100,toolbar=no,location=no,directories=no,menubar=no,scrollbars=no,resizable=no,'); return false; } 
+
+function closepopup() {
+  window.close();
 }
+
 
